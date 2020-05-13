406e39ccc8c5ae93cffa8aa5bca4a32010b0480e
cathjime <xcatherine.jimenez@gmail.com>
2020-05-13 17:03:07 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
+    def initialize(discount)
+        @total = 0
+        @discount = @total - (@total * 0.20)
+    end 
 
+    attr_accessor :total
+
+    def discount
+    end 
+
+    def add_item(title, price)
+        total += price
+
+    end 
+end 
